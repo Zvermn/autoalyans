38ad91ac0eaae2a5fd0d0b10ed0a6642aefdfc1e
Zvermn <zverevmn@yandex.ru>
2022-12-15 19:57:37 +0800

    правки поиска крошек и пагинации

--- includes/search.php
+++ includes/search.php
@@ -6,12 +6,17 @@
     <div class="search_block"></div>
     <div class="result fouls"><span></span></div>
     <div class="result true">
-        <span>Ебать находка!</span><span class="vol-result">2</span>
+        <span class="text-result">Ебать находка!</span><span class="vol-result">2</span>
         <ul class="list-results">
+            <li class="result-item">
+                <div class="result-img"><img src="/img/truck-768.jpg" alt=""></div>
+                <div class="result-search-info"><a class="link-result">Какой-то там погрузчик</a></div>
+            </li>
             <li class="result-item">
                 <div class="result-img"><img src="/img/truck-768.jpg" width="130" height="85" alt=""></div>
                 <div class="result-search-info"><a class="link-result">Какой-то там погрузчик</a></div>
             </li>
+
         </ul>
     </div>
 </div>
